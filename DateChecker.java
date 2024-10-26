3f6d960d11572bd6833406d12c735e18056c829a
OjSkillz <soojo06@gmail.com>
2024-10-26 08:54:18 +0100

    Display System Time

---
+++ DateChecker.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class DateChecker {
+  public static void main(String[] args) {
+    
+      Date now = new Date();
+      
+      System.out.println("Current Date Time is: " + now);
+      
+      
+      
+        
+  
+  }
+}
