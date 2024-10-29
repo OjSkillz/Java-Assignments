5e3d2a62b7b61dc21273a60700738d20539dc69f
OjSkillz <soojo06@gmail.com>
2024-10-29 13:29:31 +0100

    Sum of First 10 Natural Numbers

---
+++ Assignment3/SumOfFirstTenNaturalNumbers/SumOfFirstTenNaturalNumbers.java
@@ -0,0 +1,14 @@
+
+public class SumOfFirstTenNaturalNumbers {
+  public static void main(String[] args) {
+  
+  int naturalNumber = 1;
+  int sum = 1;
+  
+    while (naturalNumber < 10) {
+    naturalNumber++;
+    sum = sum + naturalNumber;
+    }
+  System.out.println("\nThe sum of the first ten natural numbers is:  " + sum);
+  }
+}
