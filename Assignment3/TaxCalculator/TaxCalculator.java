a7ea9efa1242d4c64839bd7d96d2bc11552c33bd
OjSkillz <soojo06@gmail.com>
2024-10-29 06:28:46 +0100

    Tax Calculator

---
+++ Assignment3/TaxCalculator/TaxCalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TaxCalculator {
+  public static void main(String[] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    int counter = 0;
+    
+    while (counter < 3) {
+      counter++;
+      
+      System.out.print("\nName: ");
+      String name = scanner.nextLine();
+    
+      Scanner input = new Scanner(System.in);
+      
+      System.out.print("\nEarnings in USD: ");
+      double earning = input.nextInt();
+    
+        if (earning <= 30000) {
+          double tax = (15.0 / 100) * earning;
+          System.out.printf("%nDear %s, your total tax is $%,.2f%n", name, tax);
+        } 
+          else if (earning > 30000) {
+          double tax = (20.0 / 100) * earning;
+          System.out.printf("%nDear %s, your total tax is $%,.2f%n", name, tax);
+          }   
+    }
+  
+  }
+}
