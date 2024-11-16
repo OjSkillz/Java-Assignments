c857d81bab0c48c8f164e628f2a47d340d007298
OjSkillz <soojo06@gmail.com>
2024-11-16 23:37:19 +0100

    Sum of elements in an array

--- Arrays/SumOfArrayValues/SumOfArrayValues.java
+++ Arrays/SumOfArrayValues/SumOfArrayValues.java
@@ -1,16 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class SumOfArrayValues {
   public static void main(String... args) {
-
+    Scanner input = new Scanner(System.in);
     
   int total = 0;
+  int[] myArray = new int[5];
   
-  int[] myArray = {5,4,3,2,1};
+  System.out.println("Enter " + myArray.length + " integer values: \n");
+  for (int index = 0; index < myArray.length; index++) {
+ myArray[index] = input.nextInt();
   
-  for (int value = 0; value < myArray.length; value++) {
-  
-  total = total + myArray[value];
+  total = total + myArray[index];
   }
-  System.out.println("\n" + total);
+  System.out.println("\nThe sum of all the elements of " + Arrays.toString(myArray) + " is " + total);
   }
 }
