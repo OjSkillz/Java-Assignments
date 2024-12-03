d52905ae278ca53a742ec61260f664689c1f89e9
OjSkillz <soojo06@gmail.com>
2024-12-03 15:55:52 +0100

    Academic Grading Application in Python Updated

--- PhaseGateOne/Python/academicGradingApplication.py
+++ PhaseGateOne/Python/academicGradingApplication.py
@@ -17,7 +17,8 @@ subjects = []
 
 scores_per_student = {}
 totals = []
-total = 0
+averages = []
+header = ["STUDENT", "TOT", "AVE", "POS"]
 def update_students_list(number_of_students):
     for count in range(number_of_students):
         students.append(f"Student {count + 1}")
@@ -28,41 +29,46 @@ def update_subjects(number_of_subjects):
         subjects.append(f"SUB {count+1}")
     return subjects
 
-def update_subjects_per_student(number_of_students, number_of_subjects):
-    for count in range(number_of_students):
-        update_subjects(number_of_subjects)
-        subjects_per_student.extend(subjects)
-    return subjects_per_student
+def update_totals():
+    for value in scores_per_student.values():
+        totals.append(sum(value))
+    return totals
+    
+def update_averages():
+    for value in scores_per_student.values():
+        averages.append(float(f"{sum(value)/len(value):.2f}"))
+    return averages
+    
+def display_header():
+    print("=" * 56)
+    for subject in subjects[: : -1]:
+        header.insert(1, subject)
+    for element in header:
+        print(f"{element}\t",end=" ")
+    print()
+    print("=" * 56)
 
 number_of_students = int(input("How many students do you have? >> "))
 update_students_list(number_of_students)
 
 number_of_subjects = int(input("How many subjects do they offer? >> "))
-print(update_subjects(number_of_subjects))
+update_subjects(number_of_subjects)
+
 for index in range(1, number_of_students + 1):
     print()
     print(f"Entering Scores for Student {index}") 
     scores = []
     for count in range(1, number_of_subjects + 1):
-        score = int(input(f"Enter score for Subject {count} >> "))
+        score = int(input(f"Enter score for {subjects[count - 1]} >> "))
         while score < 0 or score > 100:
             print("\nInvalid entry. Enter scores between 0 and 100")
-            score = int(input(f"Enter score for Subject {count} >> "))
+            score = int(input(f"Enter score for {subjects[count - 1]} >> "))
         scores.append(score)
         print("Saved Successfully")
+        print()
         scores_per_student[students[index-1]] = scores
-print(scores_per_student)
+print(subjects)
+update_totals()
+update_averages()
+display_header()
 
-
-
-
-for count in range(len(scores) // number_of_subjects):
-    total+= scores[count]
-totals.append(total)
-total = 0
-for count in range(len(scores) // number_of_subjects, len(scores)):
-    total+= scores[count]
-totals.append(total)
-total = 0
-
-#print(totals)
